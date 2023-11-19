12bf6e8ba9d1c8c2954e9906678194bfc85f3a9e
mmortalmxx <bankaitensazangetsu1337@gmail.com>
2023-11-19 16:02:33 +0200

    Step 2 - Create division function and set textView id.

--- app/src/main/java/com/example/debbuging/MainActivity.kt
+++ app/src/main/java/com/example/debbuging/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.debbuging
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        logging();
+        Log.d(TAG, "this is where the app crashed before")
+        val helloTextView: TextView = findViewById(R.id.hello_world)
+        Log.d(TAG, "this should be logged if the bug is fixed")
+        helloTextView.text = "Hello, debugging!"
+        logging()
+        division()
+    }
+
+    // Division Function
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(4) {
+            Thread.sleep(3000)
+            findViewById<TextView>(R.id.hello_world).setText("${numerator / denominator}")
+            denominator--
+        }
     }
 
     // Logging Function
